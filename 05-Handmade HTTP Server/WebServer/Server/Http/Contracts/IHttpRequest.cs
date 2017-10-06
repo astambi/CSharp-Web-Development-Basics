57d4bd228f3968f9d8c7d139c0a9a5122cf63cd3
Anna <annsta@gmail.com>
2017-10-06 23:12:14 +0300

    Handmade HTTP Server

---
+++ 05-Handmade HTTP Server/WebServer/Server/Http/Contracts/IHttpRequest.cs
@@ -0,0 +1,22 @@
+﻿namespace WebServer.Server.Http.Contracts
+{
+    using Enums;
+    using System.Collections.Generic;
+
+    public interface IHttpRequest
+    {
+        IDictionary<string, string> FormData { get; }
+
+        IHttpHeaderCollection Headers { get; }
+
+        string Path { get; }
+
+        HttpRequestMethod Method { get; }
+
+        string Url { get; }
+
+        IDictionary<string, string> UrlParameters { get; }
+
+        void AddUrlParameter(string key, string value);
+    }
+}
---
+++ 05-Handmade HTTP Server/WebServer/Server/Http/Contracts/IHttpResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace WebServer.Server.Http.Contracts
+{
+    using Enums;
+
+    public interface IHttpResponse
+    {
+        HttpStatusCode StatusCode { get; }
+
+        IHttpHeaderCollection Headers { get; }
+    }
+}

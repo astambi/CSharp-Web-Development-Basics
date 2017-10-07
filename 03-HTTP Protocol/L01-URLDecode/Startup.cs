01fce4e55a52f8277c4d446b051ec68fd6b556c9
Anna <annsta@gmail.com>
2017-10-07 11:45:06 +0300

    HTTP Protocol

---
+++ 03-HTTP Protocol/L01-URLDecode/Startup.cs
@@ -0,0 +1,15 @@
+﻿namespace URLDecode
+{
+    using System;
+    using System.Net;
+
+    public class Startup
+    {
+        public static void Main()
+        {
+            var urlInput = Console.ReadLine();
+            var decodedUrl = WebUtility.UrlDecode(urlInput);
+            Console.WriteLine(decodedUrl);
+        }
+    }
+}

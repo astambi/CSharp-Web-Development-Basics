c007f4de152ee4d527565fd7aa0394422a57c49c
Anna <annsta@gmail.com>
2017-10-08 01:55:29 +0300

    Web Server - By the Cake App

---
+++ 04.3-Web Server - ByTheCake App/WebServer/ByTheCakeApplication/Infrastructure/Controller.cs
@@ -0,0 +1,46 @@
+﻿namespace WebServer.ByTheCakeApplication.Infrastructure
+{
+    using Server.Enums;
+    using Server.Http.Contracts;
+    using Server.Http.Response;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+    using Views;
+
+    public abstract class Controller
+    {
+        private const string DefaultPath = @"ByTheCakeApplication\Resources\{0}.html";
+        private const string ContentPlaceholder = "{{{content}}}";
+
+        public IHttpResponse FileViewResponse(string fileName)
+        {
+            var resultHtml = ProcessFileHtml(fileName);
+
+            return new ViewResponse(HttpStatusCode.Ok, new FileView(resultHtml));
+        }
+
+        public IHttpResponse FileViewResponse(string fileName, Dictionary<string, string> values)
+        {
+            var resultHtml = ProcessFileHtml(fileName);
+
+            if (values != null && values.Any())
+            {
+                foreach (var value in values)
+                {
+                    resultHtml = resultHtml.Replace($"{{{{{{{value.Key}}}}}}}", value.Value);
+                }
+            }
+
+            return new ViewResponse(HttpStatusCode.Ok, new FileView(resultHtml));
+        }
+
+        private static string ProcessFileHtml(string fileName)
+        {
+            var layoutHtml = File.ReadAllText(string.Format(DefaultPath, "layout"));
+            var fileHtml = File.ReadAllText(string.Format(DefaultPath, fileName));
+
+            return layoutHtml.Replace(ContentPlaceholder, fileHtml);
+        }
+    }
+}

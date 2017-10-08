4a4caa695f93ca3e0d7aa07d0c736e56658a764e
Anna <annsta@gmail.com>
2017-10-09 02:56:14 +0300

    Web Server - By the Cake App

--- 04.3-Web Server - ByTheCake App/WebServer/ByTheCakeApplication/ByTheCakeApp.cs
+++ 04.3-Web Server - ByTheCake App/WebServer/ByTheCakeApplication/ByTheCakeApp.cs
@@ -8,6 +8,8 @@
     {
         public void Configure(IAppRouteConfig appRouteConfig)
         {
+            // TODO Tasks 16 - 18
+
             appRouteConfig
                 .Get("/", req => new HomeController().Index());
 
@@ -19,11 +21,43 @@
 
             appRouteConfig
                 .Post("/add",
-                req => new CakesController().Add(req.FormData["name"], req.FormData["price"]));
+                req => new CakesController().Add(
+                    req.FormData["name"], req.FormData["price"]));
 
             appRouteConfig
                 .Get("/search",
                 req => new CakesController().Search(req.UrlParameters));
+
+            appRouteConfig
+                .Get("/calculator",
+                req => new CalculatorController().Calculate());
+
+            appRouteConfig
+                .Post("calculator",
+                req => new CalculatorController().Calculate(
+                    req.FormData["number1"], req.FormData["number2"], req.FormData["mathOperator"]));
+
+            appRouteConfig
+                .Get("/login", req => new AccountController().Login());
+
+            appRouteConfig
+                .Post("/login", req => new AccountController().Login(
+                    req.FormData["username"], req.FormData["password"]));
+
+            appRouteConfig
+                .Get("/loginemail", req => new AccountController().LoginToEmail());
+
+            appRouteConfig
+                .Post("/loginemail", req => new AccountController().LoginToEmail(
+                    req.FormData["username"], req.FormData["password"]));
+
+            // Send Email TODO
+            appRouteConfig
+                .Get("/sendemail", req => new AccountController().SendEmail());
+
+            appRouteConfig
+                .Post("/sendemail", req => new AccountController().SendEmail(
+                    req.FormData["recepient"], req.FormData["subject"], req.FormData["message"]));
         }
     }
 }
---
+++ 04.3-Web Server - ByTheCake App/WebServer/ByTheCakeApplication/Controllers/AccountController.cs
@@ -0,0 +1,86 @@
+﻿namespace WebServer.ByTheCakeApplication.Controllers
+{
+    using Infrastructure;
+    using Server.Http.Contracts;
+    using System.Collections.Generic;
+
+    public class AccountController : Controller
+    {
+        public IHttpResponse Login()
+        {
+            var loginValues = new Dictionary<string, string>
+            {
+                ["result"] = string.Empty,
+            };
+
+            return this.FileViewResponse(@"account\login", loginValues);
+        }
+
+        public IHttpResponse Login(string username, string password)
+        {
+            const string LoginMessage = "Hi {0}, your password is {1}";
+            var result = string.Format(LoginMessage, username, password);
+
+            var loginValues = new Dictionary<string, string>
+            {
+                ["result"] = result,
+            };
+
+            return this.FileViewResponse(@"account\login", loginValues);
+        }
+
+        public IHttpResponse LoginToEmail()
+        {
+            var loginValues = new Dictionary<string, string>
+            {
+                ["result"] = string.Empty,
+            };
+
+            return this.FileViewResponse(@"account\login", loginValues);
+        }
+
+        public IHttpResponse LoginToEmail(string username, string password)
+        {
+            const string ValidUsername = "suAdmin";
+            const string ValidPassword = "aDmInPw17";
+            const string LoginMessageSuccess = "Hello {0}!";
+            const string LoginMessageFailure = "Invalid username or password!";
+
+            var result = LoginMessageFailure;
+            if (username == ValidUsername && password == ValidPassword)
+            {
+                result = string.Format(LoginMessageSuccess, username);
+            }
+
+            var loginValues = new Dictionary<string, string>
+            {
+                ["result"] = result,
+            };
+
+            return this.FileViewResponse(@"account\sendEmail", loginValues);
+        }
+
+        public IHttpResponse SendEmail()
+        {
+            var emailValues = new Dictionary<string, string>
+            {
+                ["result"] = string.Empty,
+            };
+
+            return this.FileViewResponse(@"account\sendEmail", emailValues);
+        }
+
+        public IHttpResponse SendEmail(string recepient, string subject, string message)
+        {
+            // todo
+
+
+            var emailValues = new Dictionary<string, string>
+            {
+                ["result"] = "TEst",
+            };
+
+            return this.FileViewResponse(@"account\sendEmail", emailValues);
+        }
+    }
+}

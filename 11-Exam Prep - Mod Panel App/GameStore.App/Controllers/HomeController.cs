4006036ff4cd8cd5f2ef534a1556d98881fddfff
Anna <annsta@gmail.com>
2017-10-28 17:21:38 +0300

    Exam Prep - Mod Panel App

--- 11-Exam Prep - Mod Panel App/GameStore.App/Controllers/HomeController.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GameStore.App.Controllers
-{
-    using SimpleMvc.Framework.Contracts;
-
-    public class HomeController : BaseController
-    {
-        public IActionResult Index() => this.View();
-    }
-}
